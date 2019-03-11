2bf1b4d26fa08c650baf71aedf2956ae481b0bc0
Josef van Niekerk <josef.van.niekerk@gmail.com>
2019-03-11 23:52:22 +0100

    Fetch and pop jokes into the list

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,35 @@
 import('../scss/app.scss');
+import config from './config';
 
-console.log('Welcome to the Chuck Norris app!');
+let appData = [];
+let jokeTemplate, jokeSection, loadButton;
+
+async function fetchRandomJokes() {
+  let result;
+  loadButton.classList.add('is-loading');
+  try {
+    result = await fetch(`${config.api.endpoint}/jokes/random/${config.api.limit}`)
+  } catch (err) {
+    console.error('Attempt to load jokes failed!');
+  }
+  appData = (await result.json()).value;
+  updateDisplay(appData);
+}
+
+function updateDisplay(appData) {
+  jokeSection.innerHTML = '';
+  appData.forEach(data => {
+    jokeSection.innerHTML += jokeTemplate.innerHTML.replace('{joke}', data.joke);
+  });
+  loadButton.classList.remove('is-loading');
+}
+
+function initApp() {
+  jokeTemplate = document.querySelector('#joke_template');
+  jokeSection = document.querySelector('#joke_section');
+  loadButton = document.querySelector('#load_button');
+
+  loadButton.addEventListener('click', fetchRandomJokes);
+}
+
+initApp();
---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export default {
+  api: {
+    endpoint: 'https://api.icndb.com',
+    limit: 10,
+  },
+}
