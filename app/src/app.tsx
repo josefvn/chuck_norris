fe71e93f3d1d1f4221962f70e83af79dd88c382a
Josef van Niekerk <josef.van.niekerk@gmail.com>
2025-02-08 16:12:42 +0200

    Remove default markup etc.

---
+++ app/src/app.tsx
@@ -0,0 +1,12 @@
+import './app.css'
+
+function App() {
+  return (
+    <>
+      <img src="/chuck_400px.png" alt="Chuck Norris" />
+      <h1>Chuck Norris Jokes</h1>
+    </>
+  )
+}
+
+export { App }
