589697f4b68f1e6e460bd0af78f6f9e83e0c8db0
Josef van Niekerk <josef.van.niekerk@gmail.com>
2025-02-08 16:12:02 +0200

    Update eslint configuration

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,28 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsEslint from 'typescript-eslint'
+import baseConfig from '../eslint.config.js'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default tsEslint.config(
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    extends: [ baseConfig ],
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+        { allowConstantExport: true }
+      ]
+    }
+  }
 )
+
---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tsEslint from 'typescript-eslint'
+
+export default tsEslint.config(
+  { ignores: [ 'dist' ] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tsEslint.configs.recommended
+    ],
+    files: [ '**/*.{js, ts,tsx}' ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    rules: {
+      'array-bracket-spacing': [ 'error', 'always' ],
+      'comma-dangle': [ 'error', 'never' ],
+      'no-console': [ 'error', { allow: [ 'warn', 'error' ] } ],
+      'object-curly-spacing': [ 'error', 'always' ],
+      'quotes': [ 'error', 'single' ],
+      'semi': [ 'error', 'never' ]
+    }
+  }
+)
