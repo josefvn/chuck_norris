ed3ab31511ea5a08f901a7b8fa5e49331581531f
Josef van Niekerk <josef.van.niekerk@gmail.com>
2025-02-08 16:56:26 +0200

    Install Tailwind CSS

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -2,10 +2,10 @@ import './app.css'
 
 function App() {
   return (
-    <>
-      <img src="/chuck_400px.png" alt="Chuck Norris" />
-      <h1>Chuck Norris Jokes</h1>
-    </>
+    <main className='flex flex-col gap-4 items-center'>
+      <img className='m-4 opacity-25' src="/chuck_400px.png" alt="Chuck Norris" />
+      <h1 className='text-gray-500 text-4xl font-bold'>Chuck Norris Jokes</h1>
+    </main>
   )
 }
 
